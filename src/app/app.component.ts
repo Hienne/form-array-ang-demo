e17e07a96242bb7df3c5248264190d84d18b11dc
hien dep trai <nguyenvanhiencntt3@gmail.com>
2022-12-18 22:36:48 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'form-array-ang-demo';
+export class AppComponent implements OnInit {
+  myForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      payInterestNum: [null],
+      dates: this.fb.array([]),
+      skills: this.fb.array([])
+    });
+  }
+
+  ngOnInit(): void {
+    this.myForm.get('payInterestNum')?.valueChanges.subscribe(data => {
+      if (!data) {
+        return;
+      }
+
+      this.dates.clear();
+      for (let i = 0; i < parseInt(data); i++) {
+        let control = new FormControl('')
+        let control2 = this.fb.group({
+          name: [''],
+          age: ['']
+        })
+
+        this.dates.push(control);
+        this.skills.push(control2);
+      }
+      
+    })
+  }
+
+  get dates() {
+    return this.myForm.controls['dates'] as FormArray;
+  }
+
+  get skills() {
+    return this.myForm.controls['skills'] as FormArray;
+  }
 }
